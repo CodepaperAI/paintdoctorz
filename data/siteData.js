215a0273a73e8dc1468ac9faefd89e0cc445468b
Vikramsingh0104 <vikramsinghparmar1400@gmail.com>
2026-07-30 23:20:22 +0530

    change the stier url in sitemap

--- data/siteData.js
+++ data/siteData.js
@@ -83,8 +83,8 @@ export const businessHours = [
   { day: "Wednesday", open: "08:00", close: "18:00" },
   { day: "Thursday", open: "08:00", close: "18:00" },
   { day: "Friday", open: "08:00", close: "18:00" },
-  { day: "Saturday", open: "09:00", close: "16:00" },
-  { day: "Sunday", open: "09:00", close: "16:00" },
+  { day: "Saturday", open: "08:00", close: "18:00" },
+  { day: "Sunday",open: "08:00", close: "18:00" },
 ];
 
 export const services = [
